505423b77f08ac173ec91e8a78ae364ee014f211
sergioft <sergiojujuy2012@gmail.com>
2025-09-20 21:47:36 -0300

    Added test classes

---
+++ src/MainBryan.java
@@ -0,0 +1,10 @@
+public class MainBryan {
+
+    public static void main(String[] args) {
+        executeProgram();
+    }
+
+    public static void executeProgram(){
+        Reward test = new Reward();
+    }
+}
---
+++ src/MainDaniel.java
@@ -0,0 +1,11 @@
+public class MainDaniel {
+
+    public static void main(String[] args) {
+        executeProgram();
+    }
+
+    public static void executeProgram(){
+        Player test = new Player();
+        
+    }
+}
