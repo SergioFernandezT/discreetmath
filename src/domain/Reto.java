fedb646f81d468b14e8e6430db6e8015e4e968f8
dannielsito1 <danielbarrios2182008@gmail.com>
2025-09-24 23:08:43 -0500

    elimine la variable oportunidades

--- src/domain/Reto.java
+++ src/domain/Reto.java
@@ -5,14 +5,13 @@ public class Reto {
     private String enunciado;
     private String[] opciones;
     private String solucion;
-    private int oportunidades;
 
-    public Reto(int idReto, String enunciado, String[] opciones, String solucion, int oportunidades) {
+
+    public Reto(int idReto, String enunciado, String[] opciones, String solucion) {
         this.idReto = idReto;
         this.enunciado = enunciado;
         this.opciones = opciones;
         this.solucion = solucion;
-        this.oportunidades = oportunidades;
     }
     public void mostrarEnunciado(){
         System.out.println("El enunciado es: " + this.enunciado);
